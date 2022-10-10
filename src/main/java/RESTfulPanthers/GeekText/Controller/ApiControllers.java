4d3e5e5430a38ee6ad5718a6321d2216b54aad74
oOminae <73506539+oOminae@users.noreply.github.com>
2022-10-10 19:47:38 -0400

    Update ApiControllers.java

--- src/main/java/RESTfulPanthers/GeekText/Controller/ApiControllers.java
+++ src/main/java/RESTfulPanthers/GeekText/Controller/ApiControllers.java
@@ -5,7 +5,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+package com.frontbackend.thymeleaf.bootstrap.model.RatingSystem;
+
 @RestController
+@RequestMapping({"/", "/Api" })
 public class ApiControllers {
+    
+    @GetMapping
+    public String main(Model model) {
+        model.addAttribute("ratingsystem", new RatingSystem());
+        return "Api";
+    }
+    
+    
 
     
