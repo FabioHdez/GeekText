82af44d9a457bb338faa566d558214bc0c816a33
Fabio Hernandez <44535804+FabioHdez@users.noreply.github.com>
2022-10-27 18:38:20 -0400

    Post request to create wishlist

--- src/main/java/RESTfulPanthers/GeekText/Controller/WishlistController.java
+++ src/main/java/RESTfulPanthers/GeekText/Controller/WishlistController.java
@@ -1,9 +1,8 @@
 package RESTfulPanthers.GeekText.Controller;
 import RESTfulPanthers.GeekText.Models.Wishlist;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import RESTfulPanthers.GeekText.Repo.WishlistRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
@@ -12,12 +11,20 @@ import java.util.UUID;
 @RequestMapping("wishlist")
 public class WishlistController {
 
+    @Autowired
+    private WishlistRepo wishlistRepo;
     @GetMapping(value = "/")
     @ResponseBody
     public Wishlist getWishlistPage(HttpServletRequest request){
         String user = request.getParameter("user");
         String name = request.getParameter("name");
         return new Wishlist(UUID.randomUUID().toString(),user,name);
+    }
+    @PostMapping(value = "/create")
+    @ResponseBody
+    public Wishlist createWishlistPage(@RequestBody Wishlist wishlist){
+        // run logic to verify if name is unique and then save to database
+        return wishlist;
     }
 }
 // Sprint 3: Here just change things from my feature and adjust it to your feature
