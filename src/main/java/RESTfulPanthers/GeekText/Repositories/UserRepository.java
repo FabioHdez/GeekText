500cb5b1aeb8290fff2fcd60308324423ac91845
oOminae <73506539+oOminae@users.noreply.github.com>
2022-11-07 17:47:44 -0500

    Update UserRepository.java

--- src/main/java/RESTfulPanthers/GeekText/Repositories/UserRepository.java
+++ src/main/java/RESTfulPanthers/GeekText/Repositories/UserRepository.java
@@ -1,5 +1,6 @@
-package Repositories;
+package RESTfulPanthers.Repositories;
 
+//Import necessary packages and features
 import Models.User;
 import org.springframework.stereotype.Repository;
 
@@ -7,15 +8,18 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+//Allows accessed data from a connected database
 @Repository
 public class UserRepository {
 
+    //Store strings
     private List<User> users = new ArrayList<>();
 
     public List<User> findAll() {
         return users;
     }
-
+    
+    //Return data based on user's ID
     public User findById(int id) {
         return users.stream()
                 .filter(user -> user.id() == id)
@@ -23,6 +27,7 @@ public class UserRepository {
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
+    //Return data based on user's name
     public User findByName(String name) {
         return users.stream()
                 .filter(user -> user.fullName().equals(name))
@@ -30,7 +35,10 @@ public class UserRepository {
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
+    //Being executed after the spring bean is initialized
     @PostConstruct
+    
+    //Store user's ID and name
     private void init() {
         users.add(new User(1,"Morgan Love"));
         users.add(new User(2,"Ezra Russell"));
