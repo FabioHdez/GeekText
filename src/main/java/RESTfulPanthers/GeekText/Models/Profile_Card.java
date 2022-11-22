6627f5dc27715026ecc587a80b7c1b9acbdcbb9f
adrod1 <101020701+adrod1@users.noreply.github.com>
2022-11-22 12:19:41 -0500

    Create Profile_Card.java

---
+++ src/main/java/RESTfulPanthers/GeekText/Models/Profile_Card.java
@@ -0,0 +1,49 @@
+package com.alex.user.rest.Models;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId( Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName( String name){
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
