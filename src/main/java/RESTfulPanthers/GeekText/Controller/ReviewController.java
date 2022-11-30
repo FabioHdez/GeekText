588123b1d0c5b841bd6bb2e94e1908787e1584f4
adrod1 <101020701+adrod1@users.noreply.github.com>
2022-11-29 21:18:14 -0500

    Create ReviewController.java

---
+++ src/main/java/RESTfulPanthers/GeekText/Controller/ReviewController.java
@@ -0,0 +1,22 @@
+package RESTfulPanthers.GeekText.Controller;
+import RESTfulPanthers.GeekText.Models.Profile;
+import RESTfulPanthers.GeekText.Models.Review;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@RequestMapping("review")
+@RestController
+public class ReviewController {
+    @GetMapping(value = "/")
+    @ResponseBody
+    public Review geReviewPage(HttpServletRequest request){
+        String book = request.getParameter("book");
+        String rating = request.getParameter("rating");
+        return new Review(UUID.randomUUID().toString(),book,rating);
+    }
+}
